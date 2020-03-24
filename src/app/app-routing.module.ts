0c0a77bdce5ea75b6054ac856a653688dfa0a7a6
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-03-24 17:33:03 +0100

    create home navbar and logout button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   { path: "", redirectTo: "/user/login", pathMatch: "full" },
   {
-    path: "user", component: UserComponent, 
+    path: "user",
+    component: UserComponent,
     children: [
       { path: "registration", component: RegistrationComponent },
-      {path: "login", component: LoginComponent}
+      { path: "login", component: LoginComponent }
     ]
-  }
+  },
+  { path: "home", component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -29,7 +29,7 @@ export class LoginComponent implements OnInit {
   onSubmit(form: NgForm) {
     this.service.login(form.value).subscribe(
       (res: any) => {
-        localStorage.setItem("token", res.token);
+        localStorage.setItem("token", res.data.token);
         this.router.navigateByUrl("/home");
       },
       err => {
